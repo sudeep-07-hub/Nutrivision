69902ad4cc936902fef6439d71c97ce313058731
Sudeep S Suvarna <sudeep2004ssuvarna@gmail.com>
2025-12-21 14:44:54 +0530

    update traing script

--- src/classification/train.py
+++ src/classification/train.py
@@ -6,32 +6,51 @@ import torch.nn as nn
 import torch.optim as optim
 from model import get_model
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu" #check if GPU is available else use CPU
+# check if GPU is available else use CPU
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 Train_data_path = "/Users/sukesh/Desktop/NutriVision/Dataset/train"
 Test_data_path = "/Users/sukesh/Desktop/NutriVision/Dataset/test"
 
 transform = transforms.Compose([
-    transforms.Resize((224,224)), #Resize image to 224x224
-    transforms.RandomHorizontalFlip(), #Imge augmentation
-    transforms.ToTensor(), #Convert image to tensor
+    transforms.Resize((224, 224)),  # Resize image to 224x224
+    transforms.RandomHorizontalFlip(),  # Imge augmentation
+    transforms.ToTensor(),  # Convert image to tensor
     transforms.Normalize(
-        mean = [0.485, 0.456, 0.406],
-        std = [0.229, 0.224, 0.225]
-    ) #Normalize image
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )  # Normalize image
 ])
 
-train_data = ImageFolder(root = Train_data_path, transform = transform)
-test_data = ImageFolder(root = Test_data_path, transform = transform)
+train_data = ImageFolder(root=Train_data_path, transform=transform)
+test_data = ImageFolder(root=Test_data_path, transform=transform)
 
-train_data_loder = DataLoader(train_data, batch_size= 16, shuffle = True)
-test_data_loder = DataLoader(test_data, batch_size= 16)
+train_data_loder = DataLoader(train_data, batch_size=16, shuffle=True)
+test_data_loder = DataLoader(test_data, batch_size=16)
 
-model = get_model(num_classes= len(test_data.classes))
+model = get_model(num_classes=len(test_data.classes))
 model.to(DEVICE)
 
+criterion = nn.CrossEntropyLoss()  # Loss function
+optimizer = optim.Adam(model.fc.parameters(), lr=0.001)  # Optimizer
 
+for epoch in range(10): #Number of epochs
 
+    model.train() # Set model to training mode
+    running_loss = 0.0 # Variable to store running loss
 
+    for images, lables in train_data_loder:
+        images, lables = images.to(DEVICE), lables.to(DEVICE) # Move images and lables to device
 
+        optimizer.zero_grad() # Zero the gradients accumulated in the previous batch
+        outputs = model(images)
+        loss = criterion(outputs, lables)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.items()
+
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_data_loder):.4f}")
+
+torch.save(model.state_dict(), "dish_classifier.pth")
 
